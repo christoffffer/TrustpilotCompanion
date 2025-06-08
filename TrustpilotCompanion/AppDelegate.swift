4a7fea6aa3fc18e1b66a4cd785e74028b7eae486
Chris Taudien <88318496+christoffffer@users.noreply.github.com>
2025-06-08 15:10:30 +0200

    Initial commit – clean slate

---
+++ TrustpilotCompanion/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  TrustpilotCompanion
+//
+//  Created by Christoffer Shokofan Taudien on 07/06/25.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
---
+++ TrustpilotCompanionTests/TrustpilotCompanionTests.swift
@@ -0,0 +1,17 @@
+//
+//  TrustpilotCompanionTests.swift
+//  TrustpilotCompanionTests
+//
+//  Created by Christoffer Shokofan Taudien on 07/06/25.
+//
+
+import Testing
+@testable import TrustpilotCompanion
+
+struct TrustpilotCompanionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
